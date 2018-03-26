dcd9c0a5012b61f587f2c82c368ae99350677605
gohnigel <gohnigel.ng@gmail.com>
2018-03-26 09:09:54 +0800

    Changed resolution size, increased gravity, prevented object from going above the screen and separated Javascript file

--- game.js
+++ game.js
@@ -12,8 +12,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 1300;
-        this.canvas.height = 600;
+        this.canvas.width = 640;
+        this.canvas.height = 480
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
